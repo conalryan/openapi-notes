15cb419990988f71d1a7ebe679522e2f7bf99b05
cryan <conal.ryan@amadeus.com>
2020-07-31 11:10:55 -0400

    .

---
+++ contacts/README.md
@@ -0,0 +1,3 @@
+# contacts
+
+`swagger-codegen generate -i contacts_oas_3.0.2.yml -l jaxrs-cxf -o out -c config.json`
---
+++ contacts/out/src/main/java/com/example/model/Contact.java
@@ -0,0 +1,107 @@
+package com.example.model;
+
+import org.joda.time.LocalDate;
+import javax.validation.constraints.*;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlEnumValue;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+public class Contact   {
+  
+  @Schema(required = true, description = "")
+  private Integer id = null;
+  
+  @Schema(required = true, description = "")
+  private String name = null;
+  
+  @Schema(description = "")
+  private LocalDate creationDate = null;
+ /**
+   * Get id
+   * @return id
+  **/
+  @JsonProperty("id")
+  @NotNull
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public Contact id(Integer id) {
+    this.id = id;
+    return this;
+  }
+
+ /**
+   * Get name
+   * @return name
+  **/
+  @JsonProperty("name")
+  @NotNull
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Contact name(String name) {
+    this.name = name;
+    return this;
+  }
+
+ /**
+   * Get creationDate
+   * @return creationDate
+  **/
+  @JsonProperty("creationDate")
+  public LocalDate getCreationDate() {
+    return creationDate;
+  }
+
+  public void setCreationDate(LocalDate creationDate) {
+    this.creationDate = creationDate;
+  }
+
+  public Contact creationDate(LocalDate creationDate) {
+    this.creationDate = creationDate;
+    return this;
+  }
+
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class Contact {\n");
+    
+    sb.append("    id: ").append(toIndentedString(id)).append("\n");
+    sb.append("    name: ").append(toIndentedString(name)).append("\n");
+    sb.append("    creationDate: ").append(toIndentedString(creationDate)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private static String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
